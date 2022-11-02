5a64d0c5ef3d807a28c30cb0d08fd1989cb4990d
nguemechieu <nguemechieu@live.com>
2022-11-02 05:38:42 +0000

    news

---
+++ main.py
@@ -0,0 +1,8 @@
+
+
+
+    # TC : O(N)
+    # SC : O(N)
+    # Runtime: 1 ms, faster than 100.00% of Python online submissions for Binary Search.
+    # Memory Usage: 13.2 MB, less than 100.00% of Python online submissions for Binary Search.bb
+
--- users.py
+++ users.py
@@ -1,3 +1,18 @@
 
 
-class user
+class user (object):
+
+
+    def __init__ (self):
+        self.username = None
+        self.password = None
+        self.email = None
+        self.first_name = None
+        self.last_name = None
+        self.middle_name = None
+        self.is_active = None
+        self.is_staff = False
+        self.is_superuser = False
+        self.groups = []
+        self.user_permissions = []
+        self.user_permissions_groups = []
